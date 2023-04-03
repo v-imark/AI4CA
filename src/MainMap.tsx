2039b30d7a924ae222b67bba075430e3b752e363
vicim994 <72468847+vicim994@users.noreply.github.com>
2023-04-03 10:52:37 +0200

    Added map files

---
+++ src/MainMap.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+import { MAPBOX_TOKEN } from "./mapToken";
+
+function MainMap() {
+  const TOKEN = MAPBOX_TOKEN;
+  return <Box></Box>;
+}
+
+export default MainMap;
--- src/Mapcontainer.tsx
+++ src/Mapcontainer.tsx
@@ -1,4 +1,5 @@
 import Box from "@mui/material/Box";
+import MainMap from "./MainMap";
 
 //
 import Userfilters from "./Userfilters";
@@ -13,16 +14,22 @@ function Mapcontainer() {
         display: "flex",
       }}
     >
-      <Box sx={{ height: "100%", width: "30%", display: "flex", flexDirection: "column"}}>
-        <Box sx={{height: "40%", width: "100%"}}>
-        </Box>
-        <Box sx={{height: "60%", width: "100%"}}>
+      <Box
+        sx={{
+          height: "100%",
+          width: "30%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Box sx={{ height: "40%", width: "100%" }}></Box>
+        <Box sx={{ height: "60%", width: "100%" }}>
           <Userfilters></Userfilters>
         </Box>
       </Box>
 
       <Box sx={{ width: "70%", backgroundColor: "lightgray" }}>
-        <p>Map here :)</p>
+        <MainMap />
       </Box>
     </Box>
   );
